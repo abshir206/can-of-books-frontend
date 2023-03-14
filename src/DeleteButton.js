328d2fd41a46d95e03fc90cd7713d6cf7c6cf653
Abshir Ibrahim <abshir.ibrahim@outlook.com>
2023-03-14 15:30:11 -0700

    delete button

---
+++ src/DeleteButton.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import "./DeleteButton.css"
+
+
+class DeleteButton extends React.Component {
+
+  render() {
+    return(
+      <>
+      <Button id = "DeleteButton" > Delete Book </Button>
+      
+      
+      
+      </>
+
+    )
+
+  }
+
+}
+export default DeleteButton;
+
+
+
